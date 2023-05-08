b3c01d69a0c1465173e463d5100231bbcb6cf8a8
maxdesalle <max@sablier.finance>
2023-05-08 14:08:34 +0200

    refactor: change header

--- src/components/HomepageHeader.tsx
+++ src/components/HomepageHeader.tsx
@@ -6,18 +6,18 @@ import React from "react";
 import styles from "./HomepageHeader.module.css";
 
 export default function HomepageHeader() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<header className={clsx("hero hero--primary", styles.heroBanner)}>
-			<div className="container">
-				<h1 className="hero__title">{siteConfig.title}</h1>
-				<p className="hero__subtitle">The protocol for real-time finance.</p>
-				<div className={styles.buttons}>
-					<Link className="button button--primary button--lg" to="/protocol/introduction">
-						Learn More
-					</Link>
-				</div>
-			</div>
-		</header>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">The cryptoasset streaming protocol.</p>
+        <div className={styles.buttons}>
+          <Link className="button button--primary button--lg" to="/protocol/introduction">
+            Learn More
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
